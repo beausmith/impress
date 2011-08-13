e8ed34204e61ba4754f5c31e2eb826da3772c780
Beau Smith <beau@beausmith.com>
2011-08-13 16:44:30 -0700

    Configure Compass

---
+++ config.rb
@@ -0,0 +1,11 @@
+# Compass Configuration
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "public"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+output_style = :nested
+relative_assets = true
+line_comments = false
+preferred_syntax = :sass
